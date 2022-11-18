2237200ddb88131b69cf4f5abe1c5c0fa45d0dfa
rhit-olingejj <olingejj@rose-hulman.edu>
2022-11-18 15:12:53 -0500

    all set for adding page main functionality

--- front-end/public/scripts/mainPage.js
+++ front-end/public/scripts/mainPage.js
@@ -5,6 +5,15 @@ var rhit = rhit || {};
 rhit.main = function () {
 
 	const inputFile = document.querySelector("#formFileMultiple");
+	const inputNameEl = document.querySelector("#inputName");
+	const inputDateEl = document.querySelector("#datepicker");
+	const inputEventEl = document.querySelector("#inputEvent");
+	const inputNamesEl = document.querySelector("#people");
+
+	document.querySelector("#addButton").onclick=(event)=>{
+		document.querySelector("#people").appendChild(document.createElement("input"));
+	};
+
 	document.querySelector("#submitButton").onclick = (event) => {
 		console.log(`Pictures submitted: ${inputFile.value} `);
 		const userAction = async () => {
@@ -25,6 +34,32 @@ rhit.main = function () {
 			}
 		    }
 			)};	
+			const names=[];
+			var i=0;
+			inputNamesEl.childNodes.forEach(value=>{
+				if(i!=0){
+					names.push(value.value);	
+				}
+				i++;
+			})
+			const sendTags = async () => {
+				const obj={name:`${inputNameEl.value}`,event:`${inputEventEl.value}`,date:`${inputDateEl.value}`,names:names}
+				console.log(obj);
+				const response = await fetch("http://localhost:8080/files", {
+					method: "post",
+					body: JSON.stringify(obj),
+				}).then(function(value){
+				console.log(value.status);
+				// do something with myJson
+				if( value.status==200){
+					location.href="mainPage.html";
+				}else{
+					location.href="fail.html";
+				}
+				}
+				)};	
+
+			sendTags();
 		  userAction();
 		}	 
 	}
